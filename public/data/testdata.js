9ae83f7007d65f24a24e4a3a2816e7211cf73914
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-05-05 14:59:33 +0100

    No map tiles (none needed), now starting to bring in data

---
+++ public/data/testdata.js
@@ -0,0 +1,156 @@
+var testData = {};
+
+testData.summary = {
+  numberOfResults: 643,
+  totalNumberOfConstituencies: 650,
+  totalVotes: 30388065,
+  forecastWinningParty: "Conservatives",
+  forecastMajority: 12
+};
+
+testData.detailsByParty = [
+  {
+    partyResults: [
+      {
+        name: "Name",
+        value: "Conservatives"
+      },
+      {
+        name: "Seats",
+        value: 326
+      },
+      {
+        name: "Gains",
+        value: 60
+      },
+      {
+        name: "Losses",
+        value: 11
+      },
+      {
+        name: "Total Votes",
+        value: 1000000
+      },
+      {
+        name: "% Share",
+        value: 36.88
+      },
+      {
+        name: "% Change",
+        value: "+0.42"
+      },
+      {
+        name: "Forecast Seats",
+        value: "300"
+      }
+    ]
+  },
+  {
+    partyResults: [
+      {
+        name: "Name",
+        value: "Labour"
+      },
+      {
+        name: "Seats",
+        value: 230
+      },
+      {
+        name: "Gains",
+        value: 6
+      },
+      {
+        name: "Losses",
+        value: 79
+      },
+      {
+        name: "Total Votes",
+        value: 1000000
+      },
+      {
+        name: "% Share",
+        value: 36.88
+      },
+      {
+        name: "% Change",
+        value: "+0.42"
+      },
+      {
+        name: "Forecast Seats",
+        value: "300"
+      }
+    ]
+  },
+  {
+    partyResults: [
+      {
+        name: "Name",
+        value: "Scottish National Party"
+      },
+      {
+        name: "Seats",
+        value: 56
+      },
+      {
+        name: "Gains",
+        value: 37
+      },
+      {
+        name: "Losses",
+        value: 0
+      },
+      {
+        name: "Total Votes",
+        value: 1000000
+      },
+      {
+        name: "% Share",
+        value: 36.88
+      },
+      {
+        name: "% Change",
+        value: "+0.42"
+      },
+      {
+        name: "Forecast Seats",
+        value: "300"
+      }
+    ]
+  },
+  {
+    partyResults: [
+      {
+        name: "Name",
+        value: "Liberal Democrat"
+      },
+      {
+        name: "Seats",
+        value: 8
+      },
+      {
+        name: "Gains",
+        value: 0
+      },
+      {
+        name: "Losses",
+        value: 47
+      },
+      {
+        name: "Total Votes",
+        value: 1000000
+      },
+      {
+        name: "% Share",
+        value: 36.88
+      },
+      {
+        name: "% Change",
+        value: "+0.42"
+      },
+      {
+        name: "Forecast Seats",
+        value: "300"
+      }
+    ]
+  }
+]
--- views/main.js
+++ views/main.js
@@ -19,7 +19,21 @@ helpers.loadTemplates(templatesUrl).then(function(templates){
     CardTemplates[key] = templates[key];
   };
 
-  console.log(CardTemplates)
+  console.log(CardTemplates);
 
-  hyperdom.append(document.body, new App());
+  var paDataUrl = '/pa/results/list?test=yes';
+  var paDataUrl = '/pa/results/get/Test_Snap_General_Election_All_SOP_102?test=yes';
+  http.get(paDataUrl)
+  .then(function(res) {
+    console.log(res.body);
+    PaData.Ge2017_SOP = res.body.FirstPastThePostStateOfParties;
+    PaData.constituencyData = {};
+    return http.get('pa/results/get/Test_Snap_General_Election_result_Aberavon_1?test=yes')
+  }).then(function(res) {
+    console.log(res.body)
+    var election = res.body.FirstPastThePostResult.Election[0];
+    PaData.constituencyData[election.Constituency[0].$.number] = election.Constituency;
+
+    hyperdom.append(document.body, new App());
+  })
 });
