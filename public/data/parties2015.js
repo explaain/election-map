807080ef4b80e8221f0ec9517d76d4901250efc6
Igor Pavlov <igor@pavlov>
2017-05-26 14:38:15 +0100

    Added real 2015 data to parties file

--- public/data/parties2015.js
+++ public/data/parties2015.js
@@ -1,106 +1,35 @@
 //TODO: Jeremy! This is not a real data yet!!!
 module.exports = [
   {
-    "party": "labour",
-    "votes": "2221877",
-    "name": "Labour",
-    "seats": "333",
-    "share": "41.37",
-    "shareChange": "+9.95"
-  },
-  {
     "party": "conservative",
-    "votes": "1841296",
+    "votes": "11334576",
     "name": "Conservative",
-    "seats": "36",
-    "share": "34.28",
-    "shareChange": "-3.50"
+    "seats": "331",
+    "share": "36.9",
+    "shareChange": "+0.8"
   },
   {
-    "party": "liberal-democrat",
-    "votes": "409022",
-    "name": "Liberal Democrat",
-    "seats": "1",
-    "share": "7.62",
-    "shareChange": "-1.04"
-  },
-  {
-    "party": "green",
-    "votes": "309601",
-    "name": "Green",
-    "seats": "1",
-    "share": "5.76",
-    "shareChange": "+1.99"
-  }/*,
-  {
-    "party": "uk-independence-party",
-    "votes": "263358",
-    "name": "UK Independence Party",
-    "seats": "1",
-    "share": "4.90",
-    "shareChange": "-7.97"
+    "party": "labour",
+    "votes": "9347304",
+    "name": "Labour",
+    "seats": "232",
+    "share": "30.4",
+    "shareChange": "+1.5"
   },
   {
     "party": "scottish-national-party",
-    "votes": "131068",
+    "votes": "1454436",
     "name": "Scottish National Party",
-    "seats": "6",
-    "share": "2.44",
-    "shareChange": "-0.34"
+    "seats": "56",
+    "share": "4.7",
+    "shareChange": "+3.1"
   },
   {
-    "party": "others",
-    "votes": "70409",
-    "name": "Others",
-    "seats": 0,
-    "share": "1.31"
-  },
-  {
-    "party": "plaid-cymru",
-    "votes": "43689",
-    "name": "Plaid Cymru",
-    "seats": 0,
-    "share": "0.81",
-    "shareChange": "+0.13"
-  },
-  {
-    "party": "democratic-unionist-party",
-    "votes": "29319",
-    "name": "Democratic Unionist Party",
-    "seats": "1",
-    "share": "0.55",
-    "shareChange": "+0.10"
-  },
-  {
-    "party": "sinn-fein",
-    "votes": "22798",
-    "name": "Sinn Fein",
-    "seats": 0,
-    "share": "0.42",
-    "shareChange": "+0.06"
-  },
-  {
-    "party": "social-democratic-and-labour-party",
-    "votes": "18969",
-    "name": "Social Democratic and Labour Party",
-    "seats": "1",
-    "share": "0.35",
-    "shareChange": "+0.26"
-  },
-  {
-    "party": "alliance",
-    "votes": "5237",
-    "name": "Alliance",
-    "seats": 0,
-    "share": "0.10",
-    "shareChange": "-0.22"
-  },
-  {
-    "party": "ulster-unionist-party",
-    "votes": "4221",
-    "name": "Ulster Unionist Party",
-    "seats": 0,
-    "share": "0.08",
-    "shareChange": "-0.03"
-  }*/
+    "party": "liberal-democrat",
+    "votes": "2415862",
+    "name": "Liberal Democrat",
+    "seats": "8",
+    "share": "7.9",
+    "shareChange": "-15.2"
+  }
 ];
