e51b19977db9de3bb90b0e811daffda8352b4cfa
Igor Pavlov <pavlov.org@gmail.com>
2017-06-08 21:59:33 +0100

    debug

--- services/pa.js
+++ services/pa.js
@@ -72,6 +72,9 @@ module.exports = function(app){
     if(!paFetchModeIsLive){
       helpers.consoleWarning("PU: Warning! Using TEST query until the START of election calculation")
     }
+    console.log("1: " + lastFetchedSopt);
+    console.log("2: " + conf.sopFetchTimeout);
+    console.log("3: " + sopFetchStatus);
     if((new Date())-lastFetchedSopt>conf.sopFetchTimeout*1000 && sopFetchStatus!=="updating"){
       lastFetchedSopt = new Date();
       const parseString = require('xml2js').parseString;
